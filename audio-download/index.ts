3911be87e26faca8f95c8ff7696cd6b792f65ebd
DanielSharp01 <DanielSharp01@gmail.com>
2021-07-14 11:00:56 +0200

    Build and link support, supress warning, program message

--- audio-download/index.ts
+++ audio-download/index.ts
@@ -2,6 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import https from 'https';
 
+process.exit(0);
+
 function downloadOgg(source) {
     const file = fs.createWriteStream(path.join(__dirname, `ogg/${source.name}.ogg`));
     https.get(source.content, function (response) {
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
+#!/usr/bin/env node
+
 import { Logger } from 'tslog';
 import { GameClient } from './GameClient';
 import { getNextAnnoucementEvent, getVoiceLineFromEvent } from './voiceLineMapper';
 import { VoiceService } from './VoiceService';
+import chalk from 'chalk';
+import { version } from '../package.json';
+
+// Suppress warning since we do not want to install the cert for the LiveClientAPI
+const originalEmitWarning = process.emitWarning
+process.emitWarning = (warning, options) => {
+    if (warning && warning.includes && warning.includes('NODE_TLS_REJECT_UNAUTHORIZED')) {
+        return
+    }
+
+    return originalEmitWarning.call(process, warning, options)
+}
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+console.log(chalk.bold.blueBright(`External LoL Announcer v${version}`));
 
 const log: Logger = new Logger({ name: "Main", displayInstanceName: false, displayFilePath: 'hidden', displayFunctionName: false });
 
